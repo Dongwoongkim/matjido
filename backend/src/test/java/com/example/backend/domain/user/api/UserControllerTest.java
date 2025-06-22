c0cf57133716e4dcc4faa34976b3785c2ee49f64
Dongwoongkim <kdo6301@jbnu.ac.kr>
2025-06-22 17:43:39 +0900

    remove : test code 임시 주석처리

--- backend/src/test/java/com/example/backend/domain/user/api/UserControllerTest.java
+++ backend/src/test/java/com/example/backend/domain/user/api/UserControllerTest.java
@@ -1,21 +1,17 @@
 package com.example.backend.domain.user.api;
 
-import com.example.backend.domain.user.service.response.UserInfoResponse;
-import org.junit.jupiter.api.Test;
-import org.springframework.web.client.RestClient;
-
 class UserControllerTest {
 
-    RestClient restClient = RestClient.create("http://localhost:8080");
-
-    @Test
-    void read() {
-        UserInfoResponse response = restClient.get()
-            .uri("/api/user/{memberId}", 10L)
-            .retrieve()
-            .body(UserInfoResponse.class);
-
-        System.out.println("response.email() = " + response.email());
-    }
+//    RestClient restClient = RestClient.create("http://localhost:8080");
+//
+//    @Test
+//    void read() {
+//        UserInfoResponse response = restClient.get()
+//            .uri("/api/user/{memberId}", 10L)
+//            .retrieve()
+//            .body(UserInfoResponse.class);
+//
+//        System.out.println("response.email() = " + response.email());
+//    }
 
 }
