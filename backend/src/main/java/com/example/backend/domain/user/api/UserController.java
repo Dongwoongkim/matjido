58265d7b70f35a9ce753ae4d2d7cfdca4bc539f1
Dongwoongkim <kdo6301@jbnu.ac.kr>
2025-06-30 19:45:58 +0900

    refactor : user api end-point uri 수정

--- backend/src/main/java/com/example/backend/domain/user/api/UserController.java
+++ backend/src/main/java/com/example/backend/domain/user/api/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
 
