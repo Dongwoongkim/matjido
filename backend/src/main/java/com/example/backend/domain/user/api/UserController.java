cbd1fe4f6aa7644eaf57fbaadd306a29956146e6
Dongwoongkim <kdo6301@jbnu.ac.kr>
2025-07-02 21:51:58 +0900

    feature : 유저 id 파라미터 바인딩 annotation 생성

--- backend/src/main/java/com/example/backend/domain/user/api/UserController.java
+++ backend/src/main/java/com/example/backend/domain/user/api/UserController.java
@@ -1,7 +1,12 @@
 package com.example.backend.domain.user.api;
 
 import com.example.backend.domain.user.service.UserService;
+import com.example.backend.domain.user.service.response.UserInfoResponse;
+import com.example.backend.global.annotation.CurrentUserId;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/info")
+    public ResponseEntity<UserInfoResponse> getInfo(@CurrentUserId Long userId) {
+        return new ResponseEntity<>(userService.getInfo(userId), HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/example/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/example/backend/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.backend.domain.user.service;
 
 import com.example.backend.domain.user.repository.UserRepository;
+import com.example.backend.domain.user.service.response.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserInfoResponse getInfo(Long userId) {
+        return UserInfoResponse.from(
+            userRepository.findById(userId)
+                .orElseThrow()
+        );
+    }
 }
