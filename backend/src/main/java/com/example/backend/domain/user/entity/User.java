b22123b018922dc6466ae6857aa516ecc79372b9
daily1313 <kimsb7218@naver.com>
2025-07-14 06:34:05 +0900

    refactor: change user id column name

--- backend/src/main/java/com/example/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/example/backend/domain/user/entity/User.java
@@ -21,7 +21,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    private Long memberId;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     private Role role;
