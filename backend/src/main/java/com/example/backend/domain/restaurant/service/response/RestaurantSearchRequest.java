4047af20d88b68b130972a6b1c23451e9b5b2246
daily1313 <kimsb7218@naver.com>
2025-07-13 17:42:31 +0900

    refactor: modify request dto format

--- backend/src/main/java/com/example/backend/domain/restaurant/service/response/RestaurantSearchRequest.java
+++ backend/src/main/java/com/example/backend/domain/restaurant/service/response/RestaurantSearchRequest.java
@@ -2,6 +2,6 @@ package com.example.backend.domain.restaurant.service.response;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RestaurantSearchRequest (
-        @NotBlank String query
-) {}
+public record RestaurantSearchRequest (@NotBlank String query) {
+
+}
