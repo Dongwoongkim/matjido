87b620faa0b04928500549b887941b7154bb4730
Dongwoongkim <kdo6301@jbnu.ac.kr>
2025-08-17 17:12:01 +0900

    refactor : restarunt exception handler 수정 및 RestaurantNotFoundException handler 추가

--- backend/src/main/java/com/example/backend/domain/restaurant/exception/handler/RestaurantExceptionHandler.java
+++ backend/src/main/java/com/example/backend/domain/restaurant/exception/handler/RestaurantExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.backend.domain.restaurant.exception.handler;
 
 import com.example.backend.domain.restaurant.exception.CategoryDepthExceededException;
 import com.example.backend.domain.restaurant.exception.CategoryNotFoundException;
+import com.example.backend.domain.restaurant.exception.RestaurantNotFoundException;
 import com.example.backend.global.response.ErrorResponse;
 import com.example.backend.global.util.ErrorResponseUtil;
 import org.springframework.http.HttpStatus;
@@ -13,12 +14,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestaurantExceptionHandler {
 
     @ExceptionHandler(CategoryDepthExceededException.class)
-    public ResponseEntity<ErrorResponse> handleCategoryDepthExceededException(RuntimeException e) {
+    public ResponseEntity<ErrorResponse> handleCategoryDepthExceededException(CategoryDepthExceededException e) {
         return ErrorResponseUtil.getResponse(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCategoryNotFoundException(CategoryNotFoundException e) {
+        return ErrorResponseUtil.getResponse(e, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleRestaurantNotFoundException(RestaurantNotFoundException e) {
         return ErrorResponseUtil.getResponse(e, HttpStatus.NOT_FOUND);
     }
 }
