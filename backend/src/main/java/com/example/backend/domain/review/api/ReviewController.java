40f2825032ecb5633fa9e70d5031de3c5d002acc
Dongwoongkim <kdo6301@jbnu.ac.kr>
2025-09-07 15:33:04 +0900

    refactor : review endpoint 복수(reviews) 변경

--- backend/src/main/java/com/example/backend/domain/review/api/ReviewController.java
+++ backend/src/main/java/com/example/backend/domain/review/api/ReviewController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
